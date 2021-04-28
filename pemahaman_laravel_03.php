e2463cae2bf32eee14c31f12e3af512f59e0d934
pandamalam <lutfiharidha1337@gmail.com>
2021-04-28 20:00:57 +0700

    beautify file

--- pemahaman_laravel_03.php
+++ pemahaman_laravel_03.php
@@ -1,17 +1,18 @@
-public function getInauguratedSchool()
-    {
-        $schools = School::all();
-        $arr =[];
-        foreach($schools as $school){
-            $values =  [
-                'id' => $school->id,
-                'school_name' => $school->school_name,
-                'inaugurated_date' => $school->inaugurated_date,
-            ];
-            array_push($arr, $values); 
-        }
-        return response()->json([
-            'success' => true,
-             'data' => $arr,
-            ],200);
+<?php
+
+public function getInauguratedSchool(){
+    $schools = School::all();
+    $arr =[];
+    foreach($schools as $school){
+        $values =  [
+            'id' => $school->id,
+            'school_name' => $school->school_name,
+            'inaugurated_date' => $school->inaugurated_date,
+        ];
+        array_push($arr, $values); 
     }
+    return response()->json([
+        'success' => true,
+            'data' => $arr,
+        ],200);
+}
